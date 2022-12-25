5e5724f147c1f5ea0f38e65ed96493a0703ce727
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-25 20:58:50 +0700

    create first http method nodejs

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("node:http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World! Im Reness");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
