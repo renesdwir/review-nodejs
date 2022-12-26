8215bac9c73a2d4a7f47529d30068bab3726c5e0
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-26 19:44:06 +0700

    create route

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const http = require("node:http");
 const newModule = require("./testModule");
 const { faker } = require("@faker-js/faker");
 
-const randomName = faker.name.jobTitle();
+const randomName = faker.name.fullName();
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end(randomName);
+  if (url === "/") res.end("Selamat datang");
+  if (url === "/users") res.end(randomName);
+  else res.end("default message");
+  console.log(url);
+  //res.end(randomName);
 });
 
 server.listen(port, hostname, () => {
