e3d2627ddc5ab2918bbb6f76caf13e1d412a1cb8
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-26 19:35:12 +0700

    faker js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require("node:http");
 const newModule = require("./testModule");
+const { faker } = require("@faker-js/faker");
 
+const randomName = faker.name.jobTitle();
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end(newModule);
+  res.end(randomName);
 });
 
 server.listen(port, hostname, () => {
