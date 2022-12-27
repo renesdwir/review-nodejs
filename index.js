1c34b73da9237d12bace479070aa62a68fca9368
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-27 21:47:55 +0700

    create routers

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routers");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/user/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  if (id === "1") {
-    res.send({ id: 1, name: "renes aja", age: 20 });
-  } else if (id === "2") {
-    res.send({ id: 2, name: "dwi", age: 21 });
-  } else {
-    res.send("No data can be display");
-  }
-});
-app.get("/user", (req, res) => {
-  const name = req.query.name;
-  const age = req.query.age;
-  if (name && age) res.send(`Hai ${name}, aku tebak umurmu ${age} tahun`);
-  else {
-    res.send("something wrong");
-  }
-});
-app.post("/user", (req, res) => {
-  res.send("Got a POST request");
-});
-app.put("/user", (req, res) => {
-  res.send("Got a PUT request at /user");
-});
-app.delete("/user", (req, res) => {
-  res.send("Got a DELETE request at /user");
-});
-
+app.use(router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
---
+++ routers.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+router.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  if (id === "1") {
+    res.send({ id: 1, name: "renes aja", age: 20 });
+  } else if (id === "2") {
+    res.send({ id: 2, name: "dwi", age: 21 });
+  } else {
+    res.send("No data can be display");
+  }
+});
+router.get("/user", (req, res) => {
+  const name = req.query.name;
+  const age = req.query.age;
+  if (name && age) res.send(`Hai ${name}, aku tebak umurmu ${age} tahun`);
+  else {
+    res.send("something wrong");
+  }
+});
+router.post("/user", (req, res) => {
+  res.send("Got a POST request");
+});
+router.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+router.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+
+module.exports = router;
