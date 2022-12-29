439d9829705d312829ca767023fa0935ffa679c7
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-29 21:51:11 +0700

    add url

--- routers.js
+++ routers.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+//https://expressjs.com/en/guide/using-middleware.html
 const middleware = function (req, res, next) {
   console.log("ini middleware");
   next();
