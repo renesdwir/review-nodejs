258dc7e4d1607dc5611a8e111b6426fd6a72bb0e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-12-30 20:39:32 +0700

    route post

--- routers.js
+++ routers.js
@@ -16,5 +16,27 @@ router.get("/users", async (req, res) => {
     res.send({ message: error.message || "Internal Server Error" });
   }
 });
+router.post("/users", async (req, res) => {
+  try {
+    const { name, age, status } = req.body;
+    console.log(name, age, status);
+    if (connection) {
+      const db = connection.db();
+      const users = await db
+        .collection("users")
+        .insertOne({ name, age, status });
+      console.log("USERS >>>>", users);
+      if (users.insertedId) {
+        res.send({ message: "Data saved successfully" });
+      } else {
+        res.send({ message: "Save data failed" });
+      }
+    } else {
+      res.send({ message: "Connection database failed" });
+    }
+  } catch (error) {
+    res.send({ message: error.message || "Internal Server Error" });
+  }
+});
 
 module.exports = router;
